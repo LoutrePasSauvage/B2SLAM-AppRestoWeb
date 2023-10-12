c66bbf4573682261f5f8a5eaf54e8ed083d524a9
yermushy31 <hector.caillebottevince@limayrac.fr>
2023-10-12 15:07:03 +0200

    modified:   list.php

--- list.php
+++ list.php
@@ -20,7 +20,7 @@ $supprimer = isset($_POST['supprimer']) ? $_POST['supprimer'] : "";
 
 $deleteID = isset($_POST['deleteID']) ? $_POST['deleteID'] : "";
 $productID = isset($_POST['productID']) ? $_POST['productID'] : "";
-
+echo $deleteID;
 if (!$user) {
     echo "<p> Vous n'êtes pas connecté ! </p>";
 }
@@ -28,18 +28,29 @@ if (!$user) {
 if ($user) {
     $produits = $db->SelectDb("SELECT * FROM produit;", NULL);
     $commandes = $db->SelectDb("SELECT * FROM commande WHERE id_user=:idUser;", [":idUser" => $user['id_user']]);
-    $id_produit =  $db->SelectDb("SELECT id_produit FROM ligne, commande, user WHERE ligne.id_commande = commande.id_commande and commande.id_user = user.id_user AND user.id_user = :id_user;", [":id_user" => $user['id_user']]);
-   
-   
-    $totalHT = $db->SelectDb("SELECT SUM(total_commande) as totalHT FROM commande WHERE id_user=:id_user;", [":id_user" => $user['id_user']]);
-    $_SESSION["totalHT"] = $totalHT[0]['totalHT'];
-}
+    $lignes =  $db->SelectDb("SELECT * FROM `ligne`, user WHERE user.id_user = :id_user;", [":id_user" => $user['id_user']]);
+    
+    
+    $totalHT = $db->SelectDb("SELECT SUM(ligne.total_ligne_ht) as totalHT FROM ligne, commande WHERE ligne.id_commande=:id_commande AND id_user=:id_user;", [":id_user" => $user['id_user'], ":id_commande" =>  $commandes[0]["id_commande"]]);
+    
+
+    $total_lignes = $db->SelectDb("SELECT total_ligne_ht FROM ligne, user WHERE user.id_user=:id_user;", [":id_user" => $user['id_user']]);
+    
+    $total_ht = 0;
+    foreach($total_lignes as $value) {
+        $total_ht += $value['total_ligne_ht'];
+
+    }
+    $_SESSION["total_commande"] = $total_ht;
+} 
 //récupération du formulaire pour un INSERT dans commande 
 
 
 if ($supprimer) {
-    $db->DeleteDb("DELETE FROM ligne WHERE id_commande=:deleteID", [":deleteID" => $deleteID]);
-    $db->DeleteDb("DELETE FROM commande WHERE id_commande=:deleteID", [":deleteID" => $deleteID]);
+    $db->DeleteDb("DELETE FROM ligne WHERE id_ligne=:deleteID", [":deleteID" => $deleteID]);
+    $delete_c = $db->SelectDb("SELECT * FROM `ligne`, user WHERE user.id_user = :id_user AND ligne.id_ligne = :id_ligne", [":id_user" => $user['id_user'], ":id_ligne" => $deleteID+1]);
+    print_r($delete_c);
+    //$db->DeleteDb("DELETE FROM commande WHERE id_commande=:deleteID", [":deleteID" => $delete_c[0]['id_commande']]);
     header("Refresh:0");
 }
 
@@ -50,17 +61,17 @@ if ($ajouter) {
     
     $db->InsertDb(
         "INSERT INTO `commande` (`id_commande`, `id_user`, `id_etat`, `date`, `total_commande`, `type_conso`) VALUES (NULL, :id_user, :id_etat, :date, :total_commande, :type_conso);",
-        [":id_etat" => "1", ":date" => date('Y-m-d'), ":total_commande" => '20.2', ":type_conso" => 'extérieur', ":id_user" => $user['id_user']]
+        [":id_etat" => "1", ":date" => date('Y-m-d'), ":total_commande" => $_SESSION['totalHT'], ":type_conso" => 'extérieur', ":id_user" => $user['id_user']]
     );
-
+    sleep(1);
     // INSERT ligne 
 
-    foreach ($commandes as $value) {
+ 
         $db->InsertDb(
-            "INSERT INTO `ligne` (`id_ligne`, `id_commande`, `id_produit`, `qte`, `total_ligne_ht`) VALUES (NULL, :id_commande, :id_produit, :qte, :total_ligne_ht);",
-            [":id_commande" => $value["id_commande"],  ":id_produit" => $productID, ":qte" => '1', ":total_ligne_ht" => $_SESSION['totalHT']]
+            "INSERT INTO `ligne` (`id_ligne`, `id_commande`, `id_produit`, `qte`, `total_ligne_ht`) VALUES (NULL, :id_commande, :id_produit, :qte, NULL);",
+            [":id_commande" => $commandes[0]["id_commande"],  ":id_produit" => $productID, ":qte" => '1']
         );
-    }
+    
     header("Refresh:0");
 }
 
@@ -118,7 +129,9 @@ if ($ajouter) {
                 </div>
                 <div class="box">
                     <div class="col align-self-start">
-                        <?php foreach ($commandes as $row) {
+                        <?php foreach ($lignes as $row) {
+
+                            $the_product = $db->SelectDb("SELECT * FROM produit WHERE id_produit=:id_produit", [':id_produit' => $row['id_produit']]);
                             echo ' 
                             <form method="POST">
                             <div class="card mb-2" style="max-width: 640px;">
@@ -128,19 +141,12 @@ if ($ajouter) {
                                 </div>
                                 <div class="col-md-8">
                                 <div class="card-body" style="width: 350px;">
-                                <h5 class="card-title"> Commande N°' . $row['id_commande'] . '</h5>';
-
-                            if ($row['id_etat'] == 0) {
-                                echo '<h5 class="card-title"> extérieur </h5>';
-                            } else {
-                                echo '<h5 class="card-title"> sur place </h5>';
-                            }
-
-                            echo '     
-                                        <p class="card-text">This is a wider card with supporting text below as a
-                                            natural lead-in to
-                                            additional content. This content is a little bit longer.</p>
-                                            <input type="hidden"  name="deleteID" value="' . $row['id_commande'] . '">
+                                <h5 class="card-title"> ' . $the_product[0]['libelle'] . '</h5>
+                                
+                                <h5 class="card-title"> Commande N°' . $row['id_commande'] . '</h5>
+                                
+                                 <p class="card-text">'.$the_product[0]['libelle'].'</p>
+                                            <input type="hidden"  name="deleteID" value="' . $row['id_ligne'] . '">
                                         <input type="submit" value="Supprimer" name="supprimer" class="btn btn-danger"/>
                                     </div>
                                 </div>
@@ -155,8 +161,8 @@ if ($ajouter) {
                 <button type="button" class="btn btn-success">Commander</button>
                 <button type="button" class="btn btn-warning">Annuler</button>
                 <div class="text-white">
-                    <h1>Prix Total HT : <?php echo $_SESSION["totalHT"]; ?> $</h1>
-                    <h1>Prix Total TVA : 9.8 $</h1>
+                    <h1>Prix Total HT : <?php echo $_SESSION["total_commande"]; ?> $</h1>
+                    <h1>Prix Total TVA : <?php echo $_SESSION["total_commande"] + $_SESSION["total_commande"]*0.05; ?> $</h1>
                 </div>
             </div>
         </div>
