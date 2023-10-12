7369512afa8c18814b9ff9c126a3c14bbc8d2f8b
Ludovic <123162852+LBouzac@users.noreply.github.com>
2023-10-12 17:04:06 +0200

    Update pay.php
    icone rajout

--- pay.php
+++ pay.php
@@ -28,26 +28,12 @@ if ($user) {
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Page de payement</title>
-    <link rel="stylesheet" href="css/main.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-</head>
-<body>
+    <title>Page de paiement</title>
 
-<nav>
-    <ul class="menu">
-        <li class="menu-item">
-            <a href="index.php">
-                <img class="logoResto" src="img/logoResto.png" width=20%/>
-            </a>
-        </li>
-        <li class="menu-item a-menu"><a href="index.php">Accueil</a></li>
-        <li class="menu-item a-menu"><a href="connexion.php">Connexion</a></li>
-    </ul>
-</nav>
+<?php
+    include('header.php');
+?>
+
     <main style="padding: 15%">
 
         <div class="row g-5">
@@ -61,7 +47,7 @@ if ($user) {
                         $the_product = $db->SelectDb("SELECT * FROM produit WHERE id_produit=:id_produit", [':id_produit' => $row['id_produit']]);
                     echo "<li class=' list-group-item d-flex justify-content-between lh-sm'>
                         <div>
-                            <h6 class='my-0'>" . $the_product[0]['libelle']."</h6>
+                            <h6 class='my-0'> <i class='fa-solid fa-utensils fa-sm'></i> &ensp; &ensp; " . $the_product[0]['libelle']."</h6>
                             <small class='text-muted'>". $the_product[0]['descProduit'] ." </small>
                         </div>
                         <span class='text-muted'> ". $the_product[0]['prix_ht'] ." € </span>
@@ -126,19 +112,13 @@ if ($user) {
 
                     <hr class="my-4">
 
-                    <button class="w-25 btn btn-primary btn-lg" type="button" onclick="location.href='payConf.php'">Payer</button> &ensp; <button class="w-25 btn btn-secondary btn-lg" type="button" onclick="location.href='list.php'">Annuler</button>
+                    <button class="w-25 btn btn-primary btn-lg" type="button" onclick="location.href='payConf.php'">Payer</button> &ensp;
+                    <button class="w-25 btn btn-secondary btn-lg" type="button" onclick="location.href='list.php'">Annuler</button>
                 </form>
             </div>
         </div>
     </main>
-<footer>
-    &copy Restaurant de qualité
-</footer>
-</body>
-</html>
+<?php
+    include "footer.php";
 
-<!--Js for boot strap-->
-
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+?>
