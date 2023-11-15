1982cc67d9c414f7f9de0be68d8b424ada9c9fcf
Ludovic <123162852+LBouzac@users.noreply.github.com>
2023-11-15 18:21:35 +0100

    Update connexion.php
    style update

--- connexion.php
+++ connexion.php
@@ -4,6 +4,8 @@ include "db_connect.php";
 $dbh = db_connect();
 
 $messages = array();  // Message d'erreur
+$messagesid = array();  // Message d'erreur Identifiant
+$messagesMDP = array();  // Message d'erreur MDP
 
 $submit = isset($_POST['submit']);
 
@@ -16,11 +18,11 @@ if ($submit) {
 
     //verifie si le login est vide
     if (empty(trim($login))) {
-        $messages[] = "le login est obligatoire";
+        $messagesid[] = "le login est obligatoire";
     }
     //verifie si le mdp est vide
     if (empty(trim($password))) {
-        $messages[] = "le mot de passe est obligatoire";
+        $messagesMDP[] = "le mot de passe est obligatoire";
     }
     //verifie si le mdp est valide
     if (!$password || !$login) {
@@ -73,8 +75,24 @@ if ($submit) {
         }
     }
     ?>
-    <p>login<br /><input type="text" name="login" id="login" ></p>
-    <p>Mot De Passe<br /><input type="password" name="password" id="password" ></p>
+    <p>login<br />
+        <?php
+        if (count($messagesid) > 0) {
+
+            foreach ($messagesid as $message) {
+                echo "<p class='btn alert-warning' >" . $message . "</p> <br>";
+            }
+        }
+        ?><input type="text" name="login" id="login" ></p>
+    <p>Mot De Passe<br />
+        <?php
+        if (count($messagesMDP) > 0) {
+
+            foreach ($messagesMDP as $message) {
+                echo "<p class='btn alert-warning' >" . $message . "</p> <br>";
+            }
+        }
+        ?><input type="password" name="password" id="password" ></p>
     <button class=" btn btn-default btn-lg active" type="submit" name="submit" value="submit">Connexion</button>
 </form>
 
