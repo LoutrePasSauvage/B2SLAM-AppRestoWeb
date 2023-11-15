c1640d993f21973ba8410b212ba8a217f22e1ef0
Ludovic <123162852+LBouzac@users.noreply.github.com>
2023-11-15 19:21:05 +0100

    Update payConf.php

--- payConf.php
+++ payConf.php
@@ -1,54 +1,90 @@
+<?php
+include_once "class/database.class.php";
+include "db_connect.php";
+
+session_start();
+
+$objetConnexion = db_connect();
+$db = new Database($objetConnexion);
+
+//récupération des produits
+
+$user = $_SESSION["user"];
+
+$productID = isset($_POST['productID']) ? $_POST['productID'] : "";
+
+if (!$user) {
+    echo "<p> Vous n'êtes pas connecté ! </p>";
+}
+
+if ($user) {
+    $login = $user['login'];
+    $email = $user['email'];
+    $produits = $db->SelectDb("SELECT * FROM produit;", NULL);
+    $lignes =  $db->SelectDb("SELECT * FROM `ligne`, user WHERE user.id_user = :id_user;", [":id_user" => $user['id_user']]);
+}
+
+if (empty($_SESSION["user"])) {
+    header("Location: index.php");
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>index</title>
-    <link rel="stylesheet" href="main.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-</head>
-<body>
+    <title>Confirmation de payement</title>
 
-<nav>
-    <ul class="menu">
-        <li class="menu-item">
-            <a href="index.php">
-                <img class="logoResto" src="img/logoResto.png" width=20%/>
-            </a>
-        </li>
-        <li class="menu-item a-menu"><a href="index.php">Accueil</a></li>
-        <li class="menu-item a-menu"><a href="connexion.php">Connexion</a></li>
-    </ul>
-</nav>
+    <?php
+    include('header.php');
+    ?>
 
-<h1>Payement</h1>
+    <main style="padding: 15%">
 
-<!-- formulaire carte bleu -->
+        <div class="container">
+            <h1 class="text-success">Confirmation de payement</h1>
 
-<div class="card" style="width: 25rem;margin: 10% 35% 50px 35%">
-    <img class="card-img-top" src="img/logoResto.png" alt="Card image cap">
-    <div class="card-body">
-        <h5 class="card-title">Commande n°8451</h5>
-        <p class="card-text">Total payer : 8.5€</p>
-    </div>
-    <form method="post">
-    <ul class="list-group list-group-flush">
-        <li class="list-group-item"><p>Merci Pour Votre Commande</p></li>
-    </ul>
+            <h4 class="mb-3">adresse de payement</h4>
+                <form class="needs-validation" novalidate="">
+                    <div class="row g-3">
+                        <div class="col-sm-6">
+                            <label for="firstName" class="form-label">Utilisateur</label>
+                            <input type="text" class="form-control" id="firstName" placeholder="" value="<?= $login ?>" disabled required="">
+                            <div class="invalid-feedback">
+                                Prenom valide requis
+                            </div>
+                        </div>
 
-        <p><button type="submit" class="btn btn-primary" href=".php" >Confirmer</button></p>
+                        <div class="col-sm-6">
+                            <label for="email" class="form-label">Email </label>
+                            <input type="email" class="form-control" id="email" placeholder="exemple@limayrac.fr" disabled value="<?= $email ?>">
+                            <div class="invalid-feedback">
+                                Entrez une e-mail valide
+                            </div>
+                        </div>
+                    </div>
+                <ul class="list-group mb-3">
+                    <h4 class="mb-3">Commande N° <?= $lignes[0]['id_commande'] ?> </h4>
+                    <?php foreach ($lignes as $row) {
+                        $the_product = $db->SelectDb("SELECT * FROM produit WHERE id_produit=:id_produit", [':id_produit' => $row['id_produit']]);
+                        echo "<li class=' list-group-item d-flex justify-content-between lh-sm'>
+                        <div>
+                            <h6 class='my-0'> <i class='fa-solid fa-utensils fa-sm'></i> &ensp;" . $the_product[0]['libelle']."</h6>
+                            <small class='text-muted'>". $the_product[0]['descProduit'] ." </small>
+                        </div>
+                        <span class='text-muted'> ". $the_product[0]['prix_ht'] ." € </span>
+                    </li>"; }?>
+                    <li class='list-group-item d-flex justify-content-between'>
+                        <span>Total (en eur)</span>
+                        <strong><?= $_SESSION['total_commande'] + $_SESSION['total_commande']*0.05;  ?> €</strong>
+                    </li>
+                </ul>
 
-</div>
+                    <button class=" btn btn-success btn-lg" type="button" onclick="location.href='list.php'">Revenir a la page d'accueil</button>
 
-<footer>
-    &copy Restaurant de qualité
-</footer>
-</body>
-</html>
-
-<!--Js for boot strap-->
-
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+                </form>
+        </div>
+    </main>
+    <?php
+    include "footer.php";
+    ?>
