710f86f6c8ae2600d58ef33d7d531be7957447d6
Ludov1234 <bouzacludovic@gmail.com>
2023-11-29 20:28:39 +0100

    maj connexion API commande accepter

--- API/commande_accepter.php
+++ API/commande_accepter.php
@@ -6,7 +6,7 @@ include_once "../class/database.class.php";
 try {
     $json = "";
     $dbh = db_connect();
-    
+
     // Pour Firefox et le formatage
     header("Content-Type: application/json");
 
@@ -14,18 +14,28 @@ try {
     $db = new Database($dbh);
     $row = $db->SelectDb($sql, [":id_commande" => $_GET['id_commande']]);
 
-    $commandeUpdate = $dbh->prepare(
-        "UPDATE commande SET id_etat = :id_Etat WHERE id_commande = :id_Commande"
-    );
+    // Check if $row is not empty before accessing its elements
+    if (!empty($row) && isset($row[0]['id_etat']) && $row[0]['id_etat'] == 2) {
+        // Utilisation d'une requête préparée pour l'UPDATE
+        $commandeUpdate = $dbh->prepare("UPDATE commande SET id_etat = :id_Etat WHERE id_commande = :id_Commande");
 
-    $commandeUpdate->execute([
-        ":id_Etat" => 4,
-        ":id_Commande" => $_GET['id_commande']
-    ]);
+        // Utilisation d'une vérification d'exécution de la requête UPDATE
+        if ($commandeUpdate->execute([
+            ":id_Etat" => 4,
+            ":id_Commande" => $_GET['id_commande']
+        ])) {
+            $json = json_encode("Commande acceptée", JSON_PRETTY_PRINT);
+        } else {
+            // Fournit un message d'erreur en cas d'échec de la requête UPDATE
+            $json = json_encode(["error" => "Erreur lors de la mise à jour de la commande."], JSON_PRETTY_PRINT);
+        }
+    } else {
+        // Si l'état n'est pas 2 ou $row est vide, retourne un message d'erreur
+        $json = json_encode(["error" => "La commande n'est pas dans l'état approprié pour être mise à jour."], JSON_PRETTY_PRINT);
+    }
 
-    $json = json_encode($row, JSON_PRETTY_PRINT);
     echo $json;
 } catch (PDOException $ex) {
+    // Capture les erreurs liées à la base de données
     die("Erreur lors de la requête SQL : " . $ex->getMessage());
 }
-?>
